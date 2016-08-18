85ec7a0f7002504db1296ade8a3bc4089a369c8e
Paul Christiano <paulfchristiano@gmail.com>
2016-08-17 23:19:10 -0700

    started adding facebook queries

--- client/stuff.js
+++ client/stuff.js
@@ -1,23 +1,53 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import template from './stuff.html';
+import fbgraph from 'fbgraph'
+
+class Facebook {
+    constructor(accessToken) {
+//this.graph = Npm.require("fbgraph")
+        this.accessToken = accessToken
+        fbgraph.setAccessToken(accessToken)
+    }
+
+    query(q) {
+       //return self.graph['get'](q)
+    }
+
+}
 
 class NameHereListCtrl {
 	constructor() {}
+
+    getUser() {
+        return Meteor.user().services.facebook
+    }
+
+    query(q) {
+        var data = Meteor.sync(function(done) {
+            fbgraph.get(q, function(err, res) {
+                           done(null, res);
+                  });
+        })
+        return data.result
+    }
 
 	getUserName() {
 		if (!Meteor.user()) {
 			return '';
 		}
 
-		return Meteor.user().services.facebook.name;
+		return this.getUser().name
 	}
 
 	login() {
+        let that = this
 		Meteor.loginWithFacebook({}, function(err){
 			if (err) {
 				throw new Meteor.Error("Facebook login failed");
-			}
+            } else {
+                fbgraph.setAccessToken(that.getUser().accessToken)
+            }
 		});
 	}
 
