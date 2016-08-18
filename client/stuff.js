443aba1aac5198ced10e2f540ad32301c2858713
Stephanie Zolayvar <zolayvars@gmail.com>
2016-08-18 00:33:21 -0700

    some stuff

--- client/stuff.js
+++ client/stuff.js
@@ -1,44 +1,32 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import template from './stuff.html';
-import fbgraph from 'fbgraph'
+import fbgraph from 'fbgraph';
 
 class NameHereListCtrl {
 	constructor() {
+		var that = this;
 		this.friends = ['Paul Christiano', 'Katja Grace'];
+		//fbgraph.setAppId('1737433616546480');
+		//fbgraph.authorize({clientId: '1737433616546480', clientSecret: 'ed7070614f995ee9d8f522a66183f3fb'})
+		//fbgraph.setAppSecret('ed7070614f995ee9d8f522a66183f3fb');
 	}
 
     getUser() {
         return Meteor.user().services.facebook
     }
 
-    query(q) {
-        var data = Meteor.async(function(done) {
-            fbgraph.get(q, function(err, res) {
-                           done(null, res);
-                  });
-        })
-        return data.result
-    }
-
 	getUserName() {
 		if (!Meteor.user()) {
 			return '';
 		}
-
 		return this.getUser().name
 	}
 
 	login() {
-        let that = this
 		Meteor.loginWithFacebook({}, function(err){
 			if (err) {
 				throw new Meteor.Error("Facebook login failed");
-            } else {
-                fbgraph.setAccessToken(that.getUser().accessToken)
-                fbgraph.get("me", function(err, res){
-                    console.log(res)
-                })
             }
 		});
 	}
