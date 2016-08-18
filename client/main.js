570e52de8251310d6370df77a3b52bae0916d799
Stephanie Zolayvar <zolayvars@gmail.com>
2016-08-17 22:45:01 -0700

    Angular material and md-toolbar

--- client/main.js
+++ client/main.js
@@ -1,8 +1,11 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import nameHereList from './stuff';
+import ngMaterial from 'angular-material';
+
 
 angular.module('nameHere', [
 	angularMeteor,
 	nameHereList.name,
+	ngMaterial,
 ]);
